468717d87db453eb8c34b30784b36bdc3ec0cee8
BeatStanford <choijaeyoung38@gmail.com>
2017-05-26 10:48:45 -0700

    Added ChangeMapView() method, Updated GPS Location on Emulator to CCA; Jaeyoung Period 2

--- GoogleMapsApp/app/src/main/java/com/example/jaeyoung/googlemapsapp/MapsActivity.java
+++ GoogleMapsApp/app/src/main/java/com/example/jaeyoung/googlemapsapp/MapsActivity.java
@@ -5,12 +5,15 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.view.View;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -51,10 +54,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng f101 = new LatLng(32.958310, -117.190596);
         mMap.addMarker(new MarkerOptions().position(f101).title("Current Location: F101, CCA"));
         */
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             return;
+        } */
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("GoogleMapsApp", "Failed Permission check 1");
+            Log.d("GoogleMapsApp", Integer.toString(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)));
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
         }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("GoogleMapsApp", "Failed Permission check 2");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+        }
+
         mMap.setMyLocationEnabled(true);
     }
-}
+
+    public void changeMapView(View p) {
+        if (mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        } else if (mMap.getMapType() == GoogleMap.MAP_TYPE_SATELLITE) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+    }
+
